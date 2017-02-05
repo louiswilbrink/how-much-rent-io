6f024eb604fbdc42e4bc9c586f2704e85db345dc
Louis Wilbrink <louis.wilbrink@gmail.com>
2017-02-05 15:54:36 -0700

    - bootstrap angular app in start.html. - map /lib requests to node_modules directory.

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 var express       = require('express');
+var path          = require('path');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var config        = require('./config/configuration').getConfig();
@@ -12,7 +13,11 @@ var app           = express();
 /*********************************************************
  * Express Configuration
  *********************************************************/
+// Make all application files public.
 app.use(express.static(config.rootDir + '/web'));
+
+// Requests for library resources will hit a /lib path that maps to the node_modules directory.
+app.use('/lib', express.static(path.join(config.rootDir + '/node_modules')));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }))
